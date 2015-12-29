c5395accc5e9e87c1442422d51d1a8be82651c0a
buting <butingspace@gmail.com>
2015-12-29 15:20:36 +0800

    added helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc,char **argv)
+{
+	printf("Hello World!\n");
+	printf("========================\n");
+	printf("Hello Git,I love you!\n");
+	return 0;
+};
