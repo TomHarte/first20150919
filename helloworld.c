922d6ef0975691d1207dab1fd37f70103c3ed8f6
buting <butingspace@gmail.com>
2015-12-30 18:24:19 +0800

    modify helloworld.c to add functionality

--- helloworld.c
+++ helloworld.c
@@ -5,6 +5,12 @@ int main(int argc,char **argv)
 	printf("Hello World!\n");
 	printf("========================\n");
 	printf("Hello Git,I love you!\n");
-	getchar();
+	unsigned int a;
+	while ( a = getchar())
+	{
+		if ( a != 10){
+			printf("%c 's ascii code is :%i\n",a,a);
+		};
+	};
 	return 0;
 };
